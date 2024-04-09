8109e08a20ed94f72630a61baa67d02425d63e83
PanthamSuresh <sureshp@mountstring.com>
2024-04-09 23:24:37 +0530

    AuctionRequest

--- src/main/java/com/project/Vertex/controller/LoginController.java
+++ src/main/java/com/project/Vertex/controller/LoginController.java
@@ -8,9 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.project.Vertex.entity.Auction;
+import com.project.Vertex.entity.AuctionRequest;
 import com.project.Vertex.entity.Register;
+import com.project.Vertex.service.AuctionRequestService;
 import com.project.Vertex.service.AuctionService;
 import com.project.Vertex.service.PropertyService;
 
@@ -33,6 +37,7 @@ public class LoginController {
 	public String getIndexPage1(Model model) {
 	//model.addAttribute("register", new Register());
 		
+		//Retrive All Auction details
 		 List<Auction> auctions = auctionService.getAll();
 		 auctions.forEach(auction -> auction.setSubmissionDate(LocalDate.now()));
 	        System.out.println("Number of auctions retrieved: " + auctions.size());
@@ -41,5 +46,18 @@ public class LoginController {
 	        return "index1";
 	}	
 	
+	
+	
+	@Autowired
+    AuctionRequestService auctionRequestService;
+   
+   @PostMapping("/auctionRequest")
+   public String saveAuctionRequest(@ModelAttribute("request")AuctionRequest auctionRequest, Model model) {
+	   
+	   model.addAttribute("request", new AuctionRequest());
+	   auctionRequestService.saveAuctionRequest(auctionRequest);
+	   
+	   return "redirect:/userSuccess";
+   }
 	
 }
---
+++ src/main/java/com/project/Vertex/service/AuctionRequestService.java
@@ -0,0 +1,8 @@
+package com.project.Vertex.service;
+
+import com.project.Vertex.entity.AuctionRequest;
+
+public interface AuctionRequestService {
+	
+	AuctionRequest saveAuctionRequest(AuctionRequest auctionRequest);
+}
